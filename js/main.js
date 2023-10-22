fa6cebb807b0b686a75f5dd5d2f7f59b08cfe8ed
Lokusok <lokusokdrok@gmail.com>
2023-10-22 13:32:54 +0300

    add minimasonryjs

--- js/main.js
+++ js/main.js
@@ -1,15 +1,16 @@
-// AOS.init({
-//   disable: () => {
-//     return window.matchMedia('(max-width: 550px)').matches;
-//   }
-// });
+AOS.init({
+  disable: () => {
+    return window.matchMedia('(max-width: 550px)').matches;
+  }
+});
 
+const isMobile = window.matchMedia('(max-width: 550px').matches;
 
-// const isMobile = window.matchMedia('(max-width: 550px').matches;
-
-// if (isMobile) {
-//   const catalogList = document.querySelector('.catalog__list');
-//   const grid = new Masonry(catalogList, {
-//     itemSelector: '.catalog__list-item',
-//   });
-// }
+if (isMobile) {
+  const masonry = new MiniMasonry({
+    container: '.catalog__list',
+    gutter: 20,
+    ultimateGutter: 20,
+    baseWidth: 120,
+  })
+}
