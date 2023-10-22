8961b5c01e9aa610e2e00bf2b052ba1d3dbdfdbb
Lokusok <lokusokdrok@gmail.com>
2023-10-22 13:09:26 +0300

    first commit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-AOS.init();
+// AOS.init({
+//   disable: () => {
+//     return window.matchMedia('(max-width: 550px)').matches;
+//   }
+// });
+
+
+// const isMobile = window.matchMedia('(max-width: 550px').matches;
+
+// if (isMobile) {
+//   const catalogList = document.querySelector('.catalog__list');
+//   const grid = new Masonry(catalogList, {
+//     itemSelector: '.catalog__list-item',
+//   });
+// }
